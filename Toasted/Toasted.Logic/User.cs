8f1b15939eef1481fd61bc1f84e94f8472f08e03
Lawrence "Vayro" Biteranta <lbluck@gmail.com>
2024-02-22 13:36:52 -0800

    User class serializable
    Milk and Serial

--- Toasted/Toasted.Logic/User.cs
+++ Toasted/Toasted.Logic/User.cs
@@ -1,4 +1,5 @@
 ﻿namespace Toasted.Logic
+using System.Xml.Serialization;
 {
     public class User
     {
@@ -9,6 +10,7 @@
         public int userID { get; set; }
         public string email { get; set; }
         public Location defaultLocation { get; set; }
+        private static XmlSerializer Serializer = new XmlSerializer(typeof(User));
 
         public User(string username, string password, string firstName, string lastName, int userID, string email, Location defaultLocation)
         {
@@ -21,5 +23,29 @@
             this.defaultLocation = defaultLocation;
         }
 
-    }
+        public checkAvailability()
+        {
+            //check availability of username, send serialized object to API
+            //should call ASYNC function from an ASYNC class that returns TRUE or FALSE depending on availability by asking the API
+
+        }
+
+        public string ToString()
+        {
+
+            return SerializeXML(this);
+        }
+
+        public static string[] SerializeXML(User user)
+        {
+            var stringWriter = new StringWriter();
+            Serializer.Serialize(stringWriter, user);
+            stringWriter.Close();
+            string[] s = { stringWriter.ToString() };
+            return s;
+        }
+
+
+
+}
 }
