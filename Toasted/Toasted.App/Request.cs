6c6fc9cfd91681f0a287374e995bfc458f7353ad
Gumshoe <msmilansky@gmail.com>
2024-02-21 20:31:26 -0800

    feat: Add Toasted.App/Request.cs for Console requests to public and private APIs

---
+++ Toasted/Toasted.App/Request.cs
@@ -0,0 +1,41 @@
+namespace Toasted.App
+{
+	public static class Request
+	{
+
+		public static LogIn()
+		{
+
+		};
+
+		public static Register()
+		{
+
+		};
+
+		/* TODO
+		- Add API Key
+		*/
+
+		// https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+		private static async GetCurrentWeatherAsync(double lat,
+													double lon,
+													string appId,
+													string[] exclude = new string { "minutely", "hourly", "daily", "alerts" },
+													string[] units = new string { "standard" },
+													string lang = "en")
+		{
+			HttpClient client = new HttpClient;
+			// Turn the 'exclude' array into a comma-delimited string, i.e "minutely,hourly,daily,alerts". If there's only one value in the array, then just return that value (exclude[0]) since there's no reason to delimit one word.
+			string excludeValues = exclude.Length == 1 ? exclude[0] : exclude.Join(",", excludes);
+			// The API specifies that the default value is "standard"; There's likely no reason to change it. 
+			string unitsValues = units.Length == 1 ? units[0] : units.Join(",", units);
+			string uri = $"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={excludeValues}&units={unitValues}&appid={appId}&lang={lang}"
+			string response = await client.GetStringAsync(uri);
+			Weather currentWeather = JsonSerializer.Deserialize<Weather>(response);
+			return currentWeather;
+		}
+
+	}
+
+}
