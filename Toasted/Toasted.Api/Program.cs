b57b05deee867551bb9e24f6c8ce637743a18fdd
meryemselicioglu <98435791+meryemselici@users.noreply.github.com>
2024-02-21 23:29:31 -0600

    added connection string

--- Toasted/Toasted.Api/Program.cs
+++ Toasted/Toasted.Api/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+string connectionString = Server=tcp:mselicioglu.database.windows.net,1433;Initial Catalog=toasted;Persist Security Info=False;User ID=mselicioglu;Password=teamGreen4;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
