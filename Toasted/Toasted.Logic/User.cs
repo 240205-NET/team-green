710db8332991352577114386d59d29bb8743c100
Lawrence "Vayro" Biteranta <lbluck@gmail.com>
2024-02-22 07:27:48 -0800

    User class started

---
+++ Toasted/Toasted.Logic/User.cs
@@ -0,0 +1,18 @@
+﻿namespace Toasted.Logic
+{
+    public class User
+    {
+        public string username { get; set }
+        public string password { get; set; }
+        public string firstName { get; set; }  
+        public string lastName { get; set; }
+        public int userID { get; set; }
+        public string email { get; set; }
+        public string defaultLocation { get; set; }
+
+        public User()
+        {
+
+        }
+    }
+}
