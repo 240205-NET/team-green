e0a280d6b655b3b5dbdca7d391185d0f05558e1b
meryemselicioglu <98435791+meryemselici@users.noreply.github.com>
2024-02-23 11:57:15 -0600

    commit changes

--- Toasted/Toasted.Data/IRepository.cs
+++ Toasted/Toasted.Data/IRepository.cs
@@ -5,7 +5,7 @@ namespace Toasted.Data
     public interface IRepository
     {
         public Task<IEnumerable<User>> GetAllUsersAsync();
-        public Task<User> GetUserByUsernameAsync(int username);
+        public Task<User> GetUserByUsernameAsync(string username);
 
         public Task AddUserAsync(User user);
     }
